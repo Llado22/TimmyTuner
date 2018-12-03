e71ec29d56877dc72204a2dc60bdbaa50ef53c31
Llado22 <aleix.lladonosa@gmail.com>
2018-12-03 16:47:19 +0100

    Fragments amb l'opció de modificar layout.

--- app/src/main/java/tuners/timmy/timmytuner/ChordsFragment.java
+++ app/src/main/java/tuners/timmy/timmytuner/ChordsFragment.java
@@ -15,7 +15,8 @@ public class ChordsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chords, container, false);
+        View view = inflater.inflate(R.layout.fragment_chords, container, false);
+        return view;
     }
 
 }

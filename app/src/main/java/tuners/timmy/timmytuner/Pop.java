e50c1347dca727d1a0b5b45fd8d5f783c6dbe010
rikib <rikibeba19@gmail.com>
2019-01-08 22:57:57 +0100

    Millores v3 i v4

--- app/src/main/java/tuners/timmy/timmytuner/Pop.java
+++ app/src/main/java/tuners/timmy/timmytuner/Pop.java
@@ -1,18 +1,23 @@
 package tuners.timmy.timmytuner;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -20,32 +25,51 @@ public class Pop extends AppCompatActivity {
 
     private RecyclerView list;
     private HorizontalAdapter adapter;
-    String[] items;
+    private List<String> items;
     private Button mas;
     private Button menos;
+    private Button save;
     private TextView octava;
     private TextView selected_note;
+    private String Nota_entrada;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.popupnotes);
 
-        adapter= new HorizontalAdapter(this,new String[]{"C","C#", "D", "D#", "E","F","F#","G","G#","A","A#","B"});
+        Intent intent = getIntent();
+        if (intent!=null){
+            Nota_entrada=intent.getStringExtra("btn_name");
+        }
+
+        adapter = new HorizontalAdapter();
+
+        items = new ArrayList<>();
+        items.add(new String("C"));
+        items.add(new String("C#"));
+        items.add(new String("D"));
+        items.add(new String("D#"));
+        items.add(new String("E"));
+        items.add(new String("F"));
+        items.add(new String("F#"));
+        items.add(new String("G"));
+        items.add(new String("G#"));
+        items.add(new String("A"));
+        items.add(new String("A#"));
+        items.add(new String("B"));
 
         list=findViewById(R.id.list);
         list.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
         list.setAdapter(adapter);
 
-
         mas = findViewById(R.id.btn_mas);
         menos = findViewById(R.id.btn_menos);
+        save = findViewById(R.id.btn_save);
         octava = findViewById(R.id.oct_view);
         selected_note = findViewById(R.id.selected_note_view);
 
-        //octava.setText("2");
-        //selected_note.setText("A#" + "2");
         actualitza();
 
         mas.setOnClickListener(new View.OnClickListener() {
@@ -56,7 +80,7 @@ public class Pop extends AppCompatActivity {
                     oct++;
                 }
                 octava.setText(String.format("%d",oct));
-                actualitza();
+                selected_note.setText( method(selected_note.getText().toString()) + octava.getText().toString());
             }
         });
 
@@ -68,26 +92,89 @@ public class Pop extends AppCompatActivity {
                     oct--;
                 }
                 octava.setText(String.format("%d",oct));
-                actualitza();
+                selected_note.setText( method(selected_note.getText().toString()) + octava.getText().toString());
             }
         });
 
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent data = new Intent();
+                data.putExtra("btn_result", selected_note.getText().toString());
+                setResult(RESULT_OK,data);
+                finish();
+            }
+        });
 
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
 
         int width = dm.widthPixels;
         int height = dm.heightPixels;
 
-        getWindow().setLayout((int)(width*.8),(int)(height*.8));
-
+        getWindow().setLayout((int) (width * .8), (int) (height * .8));
     }
-    public void actualitza(){
-        Bundle extras = getIntent().getExtras();
-        if (extras !=null){
-            String nota =extras.getString("note");
-            selected_note.setText( nota + octava.getText());
+
+    class HorizontalAdapter extends RecyclerView.Adapter<HorizontalViewHolder> {
+
+
+        @NonNull
+        @Override
+        public HorizontalViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+            View view = inflater.inflate(R.layout.lista_layout, parent, false);
+            return new HorizontalViewHolder(view);
+        }
+
+
+        @Override
+        public void onBindViewHolder(@NonNull HorizontalViewHolder holder, final int position) {
+            String item = items.get(position);
+            holder.txt.setText(item);
+        }
+
+        @Override
+        public int getItemCount() {
+            return items.size();
         }
     }
+
+    class HorizontalViewHolder extends RecyclerView.ViewHolder {
+        TextView txt;
+        public HorizontalViewHolder(View itemView) {
+                super(itemView);
+                this.txt=itemView.findViewById(R.id.nota_view);
+
+                itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        int pos = getAdapterPosition();
+                        String nota = items.get(pos);
+                        selected_note.setText(nota + octava.getText());
+                        Nota_entrada=nota;
+
+                    }
+                });
+
+        }
+    }
+
+    public void actualitza(){
+        String str=method(Nota_entrada);
+        selected_note.setText( str + octava.getText().toString());
+
+    }
+
+    public String method (String str){
+        if (str!=null && str.length()>0){
+            str=str.substring(0,str.length()-1);
+        }
+        return str;
+    }
+
 }
--- app/src/main/java/tuners/timmy/timmytuner/TunerFragment.java
+++ app/src/main/java/tuners/timmy/timmytuner/TunerFragment.java
@@ -1,33 +1,95 @@
 package tuners.timmy.timmytuner;
 
 import android.content.Intent;
-import android.media.AudioManager;
-import android.media.SoundPool;
+import android.media.AudioAttributes;
+import android.media.AudioRecord;
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.widget.Toast;
 
 
-public class TunerFragment extends Fragment {
-    public SoundPool sp;
-    public SoundMeter sm;
+public class TunerFragment extends Fragment implements View.OnClickListener {
 
-    private Button btn_a;
-    private Button btn_g;
-    private Button btn_d;
-    private Button btn_b;
-    private Button btn_e6;
-    private Button btn_e1;
-    int sonido_de_rep;
+    private String sNote;
+    boolean ContinueRecording = true;
+    private AudioRecord ar = null;
+    public TextView note;
+    private int bufferSize;
+    private int SAMPLE_RATE = 4000;
+    private int btn;
+    private String LOG_TAG = "Tuner";
+    private String nota;
+    private String NotaPop;
+    private static final int EDIT_NAME = 3;
 
-    private Button btn_record;
-    private TextView note;
-    private Button btn_stop;
+    private float hsemitone = (float) Math.pow(2.0d,(float)(1d/24d));
+    //buttons
+    private Button btn_1;
+    private Button btn_2;
+    private Button btn_3;
+    private Button btn_4;
+    private Button btn_5;
+    private Button btn_6;
+    //View afinador
+    TunerView tunerView;
+    //media player
+    private MediaPlayer mp;
+
+    RecordingThread recordingThread = new RecordingThread(new RecordingThread.Listener() {
+        @Override
+        public void onAudioDataReceived(final float pitch) {
+            final String sPitch = String.format("%.2f", pitch);
+            try {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //note.setText(sPitch);
+                        processPitch(pitch);
+                        tunerView.setPitch(pitch);
+                    }
+                });
+            } catch (Exception e) {
+                Log.e("ERROR", "En setText");
+            }
+        }
+    });
+
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.e("ACTION", "Crida al onStart() tuner");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.e("ACTION", "Crida al onStop() tuner");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.e("ACTION", "Crida al onPause() tuner");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.e("ACTION", "Crida al onResume() tuner");
+    }
 
 
     @Override
@@ -36,146 +98,268 @@ public class TunerFragment extends Fragment {
         // Inflate the layout for this fragment
 
         View view = inflater.inflate(R.layout.fragment_tuner, container, false);
+
+        tunerView = (TunerView) view.findViewById(R.id.tunerView);
         note = view.findViewById(R.id.textView);
+        btn_1 = view.findViewById(R.id.btn_1);
+        btn_2 = view.findViewById(R.id.btn_2);
+        btn_3 = view.findViewById(R.id.btn_3);
+        btn_4 = view.findViewById(R.id.btn_4);
+        btn_5 = view.findViewById(R.id.btn_5);
+        btn_6 = view.findViewById(R.id.btn_6);
+        btn_1.setOnClickListener(this);
+        btn_2.setOnClickListener(this);
+        btn_3.setOnClickListener(this);
+        btn_4.setOnClickListener(this);
+        btn_5.setOnClickListener(this);
+        btn_6.setOnClickListener(this);
 
-        btn_record = view.findViewById(R.id.button);
-        btn_stop = view.findViewById(R.id.button2);
-        btn_a = view.findViewById(R.id.btn_a);
-        btn_g = view.findViewById(R.id.btn_g);
-        btn_d = view.findViewById(R.id.btn_d);
-        btn_b = view.findViewById(R.id.btn_b);
-        btn_e6 = view.findViewById(R.id.btn_e6);
-        btn_e1 = view.findViewById(R.id.btn_e1);
-
-        btn_a.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                seleccio_a();
-                rep();
-            }
-        });
-
-        btn_a.setOnLongClickListener(new View.OnLongClickListener() {
+        btn_1.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                Toast.makeText(getActivity(),"longclick",Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(getActivity(),Pop.class));
+                Intent intent = new Intent(getActivity(), Pop.class);
+                intent.putExtra("btn_name", btn_1.getText().toString());
+                startActivityForResult(intent,EDIT_NAME);
+                btn=1;
+                return false;
+            }
+        });
+        btn_2.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Intent intent = new Intent(getActivity(), Pop.class);
+                intent.putExtra("btn_name", btn_2.getText().toString());
+                startActivityForResult(intent,EDIT_NAME);
+                btn=2;
+                return false;
+            }
+        });
+        btn_3.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Intent intent = new Intent(getActivity(), Pop.class);
+                intent.putExtra("btn_name", btn_3.getText().toString());
+                startActivityForResult(intent,EDIT_NAME);
+                btn=3;
+                return false;
+            }
+        });
+        btn_4.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Intent intent = new Intent(getActivity(), Pop.class);
+                intent.putExtra("btn_name", btn_4.getText().toString());
+                startActivityForResult(intent,EDIT_NAME);
+                btn=4;
+                return false;
+            }
+        });
+        btn_5.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Intent intent = new Intent(getActivity(), Pop.class);
+                intent.putExtra("btn_name", btn_5.getText().toString());
+                startActivityForResult(intent,EDIT_NAME);
+                btn=5;
+                return false;
+            }
+        });
+        btn_6.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Intent intent = new Intent(getActivity(), Pop.class);
+                intent.putExtra("btn_name", btn_6.getText().toString());
+                startActivityForResult(intent,EDIT_NAME);
+                btn=6;
                 return false;
             }
         });
 
-        btn_g.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                seleccio_g();
-                rep();
-            }
-        });
-
-        btn_d.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                seleccio_d();
-                rep();
-            }
-        });
-
-        btn_b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                seleccio_b();
-                rep();
-            }
-        });
-
-        btn_e6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                seleccio_e6();
-                rep();
-            }
-        });
-
-        btn_e1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                seleccio_e1();
-                rep();
-            }
-        });
-
-        btn_record.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onRecord();
-            }
-        });
-
-        btn_stop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onStopRecording();
-            }
-        });
-
-        sm = new SoundMeter();
-        sm.start();
-        sp = new SoundPool(1, AudioManager.STREAM_MUSIC,1);
-
         return view;
+    }
 
-
-    }
-    public void seleccio_a(){
-        String nota = btn_a.getText().toString();
-        if (nota.equals("A")) {
-            sonido_de_rep = sp.load(getActivity(), R.raw.la, 1);
-        }
-    }
-    public void seleccio_g(){
-        String nota = btn_g.getText().toString();
-        if (nota.equals("G")){
-            sonido_de_rep=sp.load(getActivity(),R.raw.sol,1);
-        }
-    }
-    public void seleccio_d(){
-        String nota = btn_d.getText().toString();
-        if (nota.equals("D")){
-            sonido_de_rep=sp.load(getActivity(),R.raw.re,1);
-        }
-    }
-    public void seleccio_b(){
-        String nota = btn_b.getText().toString();
-        if (nota.equals("B")) {
-            sonido_de_rep = sp.load(getActivity(), R.raw.si, 1);
-        }
-    }
-    public void seleccio_e6(){
-        String nota = btn_e6.getText().toString();
-        if (nota.equals("E")){
-            sonido_de_rep=sp.load(getActivity(),R.raw.mi,1);
-        }
-    }
-    public void seleccio_e1(){
-        String nota = btn_e1.getText().toString();
-        if (nota.equals("E")){
-            sonido_de_rep=sp.load(getActivity(),R.raw.la,1);
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            case EDIT_NAME:
+                if(resultCode==-1 && btn==1){
+                    btn_1.setText(data.getStringExtra("btn_result"));
+                }
+                else if(resultCode==-1 && btn==2){
+                    btn_2.setText(data.getStringExtra("btn_result"));
+                }
+                else if(resultCode==-1 && btn==3){
+                    btn_3.setText(data.getStringExtra("btn_result"));
+                }
+                else if(resultCode==-1 && btn==4){
+                    btn_4.setText(data.getStringExtra("btn_result"));
+                }
+                else if(resultCode==-1 && btn==5){
+                    btn_5.setText(data.getStringExtra("btn_result"));
+                }
+                else if(resultCode==-1 && btn==6){
+                    btn_6.setText(data.getStringExtra("btn_result"));
+                }
+                break;
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
         }
     }
 
-    public void rep(){
-        sp.play(sonido_de_rep,1,1,1,0,0);
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            case R.id.btn_1:
+                nota = btn_1.getText().toString().toLowerCase();
+                break;
+
+            case R.id.btn_2:
+                nota = btn_2.getText().toString().toLowerCase();
+                break;
+
+            case R.id.btn_3:
+                nota = btn_3.getText().toString().toLowerCase();
+                break;
+
+            case R.id.btn_4:
+                nota = btn_4.getText().toString().toLowerCase();
+                break;
+
+            case R.id.btn_5:
+                nota = btn_5.getText().toString().toLowerCase();
+                break;
+
+            case R.id.btn_6:
+                nota = btn_6.getText().toString().toLowerCase();
+                break;
+
+        }
+        tunerView.setSelected(noteToPitch(nota.toUpperCase()));
+        try {
+            int id = this.getResources().getIdentifier(nota,"raw",getActivity().getPackageName());
+            if(id!=0){
+                mp = MediaPlayer.create(getActivity(), id);
+                mp.start();
+            } else{
+                Toast.makeText(getActivity(), nota + ": ID=" + String.valueOf(id) + " error",Toast.LENGTH_SHORT).show();
+            }
+            mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                public void onCompletion(MediaPlayer mp) {
+                    mp.release();
+                }
+            });
+        } catch (Exception e) {
+            Log.e("ERROR", "Error en media player");
+        }
+
     }
 
-    public void onRecord() {
-        Double amplitude = sm.getAmplitude();
-        String amp = amplitude.toString();
-        note.setText(amp);
+
+    //conversió nota a pitch
+    public float noteToPitch(String note){
+        String[][] notes = { {"C"}, {"Db", "C#"}, {"D"}, {"Eb", "D#"}, {"E"},
+                {"F"}, {"Gb", "F#"}, {"G"}, {"Ab", "G#"}, {"A"}, {"Bb", "A#"}, {"B"} };
+        char[] splitNote = note.toCharArray();
+        String sym = "";
+        int oct = 0;
+        float pitch = 0;
+
+        if (splitNote.length == 2) {
+            sym += splitNote[0];
+            oct = Character.getNumericValue(splitNote[1]);
+        } else if (splitNote.length == 3) {
+            sym += Character.toString(splitNote[0]);
+            sym += Character.toString(splitNote[1]);
+            oct = Character.getNumericValue(splitNote[2]);
+        }
+
+        //relació amb la 2a octava
+        int octrelation = (oct-2)*2;
+        if (oct==2){
+            octrelation = 1;
+        }
+        // Busca la nota corresponent a l'array, la pos = num de semitons de distancia amb C2 (65,41Hz)
+        for (int i = 0; i < notes.length; i++) {
+            for (int j = 0; j < notes[i].length; j++) {
+                if (notes[i][j].equals(sym)) {
+                    float semitones = (float) Math.pow(2d, (double)i / 12d);
+                    pitch = ((65.41f * semitones) * (float)octrelation);
+                    return pitch;
+                }
+            }
+        }
+        return -1;
+    }
+
+    //conversió pitch a nota
+    public void processPitch(float pitchInHz) {
+        int octave = (int) Math.floor(Math.log(pitchInHz/65.41)/Math.log(2));
+        if(octave>=1){
+            pitchInHz=pitchInHz/(octave*2);
+        }
+        if(pitchInHz >= (65.41/hsemitone) && pitchInHz < (65.41*hsemitone)) {
+            sNote = "C";
+        }
+        else if(pitchInHz >= (69.30/hsemitone) && pitchInHz < (69.30*hsemitone)) {
+            sNote = "C#";
+        }
+        else if(pitchInHz >= (73.42/hsemitone) && pitchInHz < (73.42*hsemitone)) {
+            sNote = "D";
+        }
+        else if(pitchInHz >= (77.78/hsemitone) && pitchInHz < (77.78*hsemitone)) {
+            sNote = "D#";
+        }
+        else if(pitchInHz >= (82.41/hsemitone) && pitchInHz < (82.41*hsemitone)) {
+            sNote = "E";
+        }
+        else if(pitchInHz >= (87.307/hsemitone) && pitchInHz < (87.307*hsemitone)) {
+            sNote = "F";
+        }
+        else if(pitchInHz >= (92.499/hsemitone) && pitchInHz < (92.499*hsemitone)) {
+            sNote = "F#";
+        }
+        else if(pitchInHz >= (97.999/hsemitone) && pitchInHz < (97.999*hsemitone)) {
+            sNote = "G";
+        }
+        else if(pitchInHz >= (103.826/hsemitone) && pitchInHz < (103.826*hsemitone)) {
+            sNote = "G#";
+        }
+        else if(pitchInHz >= (110/hsemitone) && pitchInHz < (110*hsemitone)) {
+            sNote = "A";
+        }
+        else if(pitchInHz >= (123.47/hsemitone) && pitchInHz < (123.47*hsemitone)) {
+            sNote = "B";
+        }
+        else {
+            sNote = "";
+        }
+
+        //octava a la que pertany
+        if(octave==0){
+            octave = 2;
+        } else {octave = octave*2;}
+
+        if(!sNote.equals("")){
+            note.setText(sNote+String.valueOf(octave));
+        } else{
+            note.setText("");
+        }
 
     }
 
-    public void onStopRecording() {
-        sm.stop();
+
+
+    public void startRecording() {
+        if (!recordingThread.recording()) {
+            recordingThread.startRecording();
+        }
+    }
+
+    public void stopRecording() {
+        if (recordingThread.recording()) {
+            recordingThread.stopRecording();
+        }
     }
 }
 
